abd2cf775b6809cd7cb6f7089eee71b81415025e
Rafael Willians <me@rwillians.com>
2023-08-22 15:02:01 -0300

    Retirei o randomized

--- user-files/simulations/rinhabackend/RinhaBackendSimulation.scala
+++ user-files/simulations/rinhabackend/RinhaBackendSimulation.scala
@@ -52,7 +52,7 @@ class RinhaBackendSimulation
   setUp(
     criacaoEConsultaPessoas.inject(
       constantUsersPerSec(2).during(10.seconds), // warm up
-      constantUsersPerSec(5).during(15.seconds).randomized, // are you ready?
+      constantUsersPerSec(5).during(15.seconds), // are you ready?
 
       rampUsersPerSec(6).to(300).during(3.minutes) // lezzz go!!!
     ),
